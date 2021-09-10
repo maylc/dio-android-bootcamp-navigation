f6a1919b72e27cbd832fd05d3dc8921ffc96e1cc
Mayara Felix <maylc.dev@gmail.com>
2021-09-09 23:25:37 -0400

    Create adapter and use it in the main activity.

---
+++ BootcampKotlinNavigation/app/src/main/java/io/github/maylcf/bootcamp/ContactAdapter.kt
@@ -0,0 +1,41 @@
+package io.github.maylcf.bootcamp
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class ContactAdapter : RecyclerView.Adapter<ContactAdapter.ContactAdapterViewHolder>() {
+
+    private val list: MutableList<Contact> = mutableListOf()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContactAdapterViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.contact_item, parent, false)
+        return ContactAdapterViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ContactAdapterViewHolder, position: Int) {
+        holder.bind(list[position])
+    }
+
+    override fun getItemCount(): Int = list.size
+
+    fun updateList(list: List<Contact>) {
+        this.list.clear()
+        this.list.addAll(list)
+        notifyDataSetChanged()
+    }
+
+    class ContactAdapterViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val name = itemView.findViewById<TextView>(R.id.contact_name)
+        private val phone = itemView.findViewById<TextView>(R.id.contact_phone)
+        private val image = itemView.findViewById<ImageView>(R.id.contact_photo)
+
+        fun bind(contact: Contact) {
+            name.text = contact.name
+            phone.text = contact.phone
+        }
+    }
+}
--- BootcampKotlinNavigation/app/src/main/java/io/github/maylcf/bootcamp/MainActivity.kt
+++ BootcampKotlinNavigation/app/src/main/java/io/github/maylcf/bootcamp/MainActivity.kt
@@ -1,11 +1,37 @@
 package io.github.maylcf.bootcamp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    private val contactList: RecyclerView by lazy {
+        findViewById(R.id.contact_list)
+    }
+
+    private val adapter = ContactAdapter()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bindView()
+        updateList()
+    }
+
+    private fun bindView() {
+        contactList.adapter = adapter
+        contactList.layoutManager = LinearLayoutManager(this)
+    }
+
+    private fun updateList() {
+        adapter.updateList(
+                arrayListOf(
+                        Contact("Mary", "111-222-3333", ""),
+                        Contact("John", "123-456-7890", "")
+                )
+        )
     }
 }
