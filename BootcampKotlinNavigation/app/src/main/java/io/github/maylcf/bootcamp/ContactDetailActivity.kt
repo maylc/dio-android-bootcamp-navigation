57262d7fe1e6779819149898c68edf73396afb5e
Mayara Felix <maylc.dev@gmail.com>
2021-09-11 08:48:23 -0400

    Create contact details activity.

---
+++ BootcampKotlinNavigation/app/src/main/java/io/github/maylcf/bootcamp/ContactDetailActivity.kt
@@ -0,0 +1,11 @@
+package io.github.maylcf.bootcamp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ContactDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_contact_detail)
+    }
+}
