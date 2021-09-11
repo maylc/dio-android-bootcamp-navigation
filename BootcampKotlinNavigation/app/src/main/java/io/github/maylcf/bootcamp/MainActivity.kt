428ea1118804d4412f8e8104cb07286728360e17
Mayara Felix <maylc.dev@gmail.com>
2021-09-10 23:00:33 -0400

    Implement drawer.

--- BootcampKotlinNavigation/app/src/main/java/io/github/maylcf/bootcamp/MainActivity.kt
+++ BootcampKotlinNavigation/app/src/main/java/io/github/maylcf/bootcamp/MainActivity.kt
@@ -3,8 +3,12 @@ package io.github.maylcf.bootcamp
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -18,10 +22,21 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.drawer_menu)
 
+        initDrawer()
         bindView()
         updateList()
+    }
+
+    private fun initDrawer() {
+        val drawerLayout = findViewById<View>(R.id.drawer_layout) as DrawerLayout
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        val toggle = ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_drawer, R.string.close_drawer)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
